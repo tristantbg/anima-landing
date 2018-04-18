b9f2df63fbfa4522b426246aa0905d2d1e84f40c
Tristan Bagot <tristan.bagot@gmail.com>
2018-04-18 12:30:19 +0200

    anima ok

--- src/app/index.js
+++ src/app/index.js
@@ -9,6 +9,14 @@
 // import 'bootstrap/js/dist/util';
 // import 'bootstrap/js/dist/dropdown';
 
+import moment from 'moment';
+import tz from 'moment-timezone';
+import jump from 'jump.js';
+import lazysizes from 'lazysizes';
+import Flickity from 'flickity';
+import TweenLite from 'gsap';
+require('../../node_modules/lazysizes/plugins/unveilhooks/ls.unveilhooks.js');
+
 // Import polyfills
 import {applyPolyfills} from './base/polyfills';
 
@@ -20,6 +28,111 @@ import '../assets/scss/index.scss';
 
 require('viewport-units-buggyfill').init();
 
+let introHidden = false;
+
+const freezeVp = (e) => {
+	e.preventDefault();
+};
+
+const stopBodyScrolling = (bool) => {
+	if (bool === true) {
+		document.body.addEventListener('touchmove', freezeVp, false);
+	} else {
+		document.body.removeEventListener('touchmove', freezeVp, false);
+	}
+};
+
+// Const easeInOutCubic = (t, b, c, d) => {
+// 	if ((t/=d/2) < 1) return c/2*t*t*t + b;
+// 	return c/2*((t-=2)*t*t + 2) + b;
+// };
+
+const easeInOutExpo = (t, b, c, d) => {
+	if (t == 0) return b;
+	if (t == d) return b + c;
+	if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+	return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+};
+
+const timeClock = () => {
+	const timeLa = document.querySelector('#la-time');
+
+	const timeFr = document.querySelector('#fr-time');
+
+	const displayTime = () => {
+		const now = moment();
+
+		const timeInLa = now.tz('America/Los_Angeles');
+		timeLa.innerHTML = 'Los Angeles<br>' + timeInLa.format('hh:mm:ss a');
+
+		const timeInFr = now.tz('Europe/Paris');
+		timeFr.innerHTML = 'Paris<br>' + timeInFr.format('hh:mm:ss a');
+	};
+
+	setInterval(displayTime, 1000);
+};
+
+const rand = (min, max) => {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const scan = () => {
+
+	const slider = document.getElementById('intro-scan');
+	const flkty = new Flickity(slider, {
+		cellSelector: '.image',
+		pageDots: false,
+		prevNextButtons: false,
+		accessibility: false
+	});
+
+	// setInterval(function() {
+	// 	flkty.next(true, true);
+	// }, 600);
+
+	const makeScan = () => {
+		const timing = rand(2, 7);
+		if (introHidden) return;
+		scanAnim(timing);
+	};
+
+	const scanAnim = (timing) => {
+		TweenLite.fromTo('#scanner-light', timing, {
+			height: 0,
+			yPercent: 0
+		}, {
+			height: '100%',
+			yPercent: 100,
+			ease: Cubic.easeInOut,
+			onComplete: makeScan
+		});
+		setTimeout(() => {
+			flkty.next(true, true);
+		}, timing);
+	};
+
+	makeScan();
+};
+
+const intro = () => {
+	window.scroll(0, 0);
+	stopBodyScrolling(true);
+	document.getElementById('intro').addEventListener('click', () => {
+		jump('#about', {
+			duration: 1000,
+			callback: hideIntro,
+			easing: easeInOutExpo
+		});
+	});
+};
+
+const hideIntro = () => {
+	document.body.classList.remove('intro');
+	document.getElementById('intro').style.display = 'none';
+	stopBodyScrolling(false);
+	introHidden = true;
+};
+
 $(async () => {
 	// Wait with further execution until needed polyfills are loaded.
 	await applyPolyfills();
@@ -27,27 +140,12 @@ $(async () => {
 	consoleErrorFix();
 	ieViewportFix();
 
-	const links = document.querySelectorAll('[link-to]');
-
-	links.forEach((element) => {
-		element.link = element.getAttribute('link-to');
-		element.addEventListener('click', () => {
-			window.location.href = element.link;
-		});
-	});
-
-	const backgrounds = document.querySelectorAll('div.lazy');
-
-	backgrounds.forEach((element) => {
-		const link = element.getAttribute('data-src');
-		element.style.backgroundImage = 'url(' + link + ')';
-		setTimeout(() => {
-			element.classList.add('loaded');
-		}, 50);
-	});
+	timeClock();
+	scan();
+	intro();
 
 	setTimeout(() => {
-		window.location.href = links[0].link;
-	}, 5000);
+		document.getElementById('loader').style.display = 'none';
+	}, 0);
 
 });
